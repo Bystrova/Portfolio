7b57d3b51ce3c0aac83158df5515a331f1136092
unknown <antonenko.a.b@gmail.com>
2022-01-07 12:39:04 +0300

    Добавлена минификация файлов и оптимизация изображений

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,30 @@
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const sourcemap = require("gulp-sourcemaps");
-const sass = require("gulp-sass")(require("sass"));
-const autoprefixer = require("gulp-autoprefixer");
-const sync = require("browser-sync").create();
-const del = require("del");
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sourcemap = require('gulp-sourcemaps');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('autoprefixer');
+const sync = require('browser-sync').create();
+const del = require('del');
+const htmlmin = require('gulp-htmlmin');
+const postcss = require('gulp-postcss');
+const csso = require('postcss-csso');
+const rename = require('gulp-rename');
+const terser = require('gulp-terser');
+const squoosh = require('gulp-libsquoosh');
 
 // Styles
 
 const styles = () => {
-	return gulp.src("source/sass/style.scss")
+	return gulp.src('source/sass/style.scss')
 		.pipe(plumber())
 		.pipe(sourcemap.init())
 		.pipe(sass())
-		.pipe(autoprefixer())
-		.pipe(sourcemap.write("."))
-		.pipe(gulp.dest("build/css"))
+		.pipe(postcss([autoprefixer(),
+			csso()
+		]))
+		.pipe(rename('style.min.css'))
+		.pipe(sourcemap.write('.'))
+		.pipe(gulp.dest('build/css'))
 		.pipe(sync.stream());
 }
 
@@ -24,8 +33,11 @@ exports.styles = styles;
 //HTML
 
 const html = () => {
-	return gulp.src("source/*.html")
-		.pipe(gulp.dest("build"))
+	return gulp.src('source/*.html')
+		.pipe(htmlmin({
+			collapseWhitespace: true
+		}))
+		.pipe(gulp.dest('build'))
 }
 
 exports.html = html;
@@ -33,17 +45,25 @@ exports.html = html;
 //Scripts
 
 const scripts = () => {
-	return gulp.src("source/scripts/*.js")
-		.pipe(gulp.dest("build/scripts"))
+	return gulp.src('source/scripts/*.js')
+		.pipe(terser())
+		.pipe(rename('script.min.js'))
+		.pipe(gulp.dest('build/scripts'))
 }
 
 exports.scripts = scripts;
 
 //Img
 
+const optimizeImages = () => {
+	return gulp.src('source/img/**/*.{png,jpg,svg}')
+		.pipe(squoosh())
+		.pipe(gulp.dest('build/img'))
+}
+
 const copyImages = () => {
-	return gulp.src("source/img/**/*.{png,jpg,svg}")
-		.pipe(gulp.dest("build/img"))
+	return gulp.src('source/img/**/*.{png,jpg,svg}')
+		.pipe(gulp.dest('build/img'))
 }
 
 exports.copyImages = copyImages;
@@ -51,7 +71,7 @@ exports.copyImages = copyImages;
 //Clean
 
 const clean = () => {
-	return del("build");
+	return del('build');
 }
 
 exports.clean = clean;
@@ -61,7 +81,7 @@ exports.clean = clean;
 const server = (done) => {
 	sync.init({
 		server: {
-			baseDir: "build"
+			baseDir: 'build'
 		},
 		cors: true,
 		notify: false,
@@ -82,17 +102,17 @@ const reload = (done) => {
 // Watcher
 
 const watcher = () => {
-	gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-	gulp.watch("source/scripts/script.js", gulp.series(scripts));
-	// gulp.watch("source/*.html").on("change", sync.reload);
-	gulp.watch("source/*.html", gulp.series(html, reload));
+	gulp.watch('source/sass/**/*.scss', gulp.series('styles'));
+	gulp.watch('source/scripts/script.js', gulp.series(scripts));
+	// gulp.watch('source/*.html').on('change', sync.reload);
+	gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
 //Build
 
 const build = gulp.series(
 	clean,
-	copyImages,
+	optimizeImages,
 	scripts,
 	gulp.parallel(
 		html,
